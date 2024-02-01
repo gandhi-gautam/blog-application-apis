4d13872db1e147b5c7daffd835a6744b4a296e3a
gautam <gautam.gandhi.100@gmail.com>
2024-02-01 07:56:10 +0530

    Added user entity

---
+++ src/main/java/com/blogapp/entities/User.java
@@ -0,0 +1,24 @@
+package com.blogapp.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "user_name", nullable = false, length = 100)
+    private String name;
+    private String email;
+    private String password;
+    private String about;
+}
